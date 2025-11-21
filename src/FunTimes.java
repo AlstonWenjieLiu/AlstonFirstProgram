23726ce94063b4cdd79ea2b7e419506c48397c1a
BT_1N3_32 <sample@bths.edu>
2025-11-21 11:01:36 -0500

    last commit??

--- src/FunTimes.java
+++ src/FunTimes.java
@@ -2,5 +2,6 @@ public class FunTimes {
     public static void main(String[] args) {
         System.out.println("Gojo the goat!");
         System.out.println("bruh");
+        System.out.println("hi");
     }
 }
