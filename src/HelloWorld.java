24732f9e098d89007ae7b5e885a55513a5652f4a
BT_1N3_32 <sample@bths.edu>
2025-11-21 10:12:55 -0500

    3 commit added Neu

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Bonjour Mundo!");
-        System.out.println("Furina my goat!");
+        System.out.println("Furina my queen!");
+        System.out.println("Neuvillete my goat also!");
     }
 }
